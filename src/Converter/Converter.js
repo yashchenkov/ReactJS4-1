3ad0b474757a10fcf84cb2a5c13ac618a80ff6b3
Yashchenko Victor <Andrey@Komp11.local>
2022-07-08 18:40:01 +0300

    final

--- src/Converter/Converter.js
+++ src/Converter/Converter.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 export default function Converter() {
   const [ hex, setHex ] = useState('');
   const [ rgb, setRgb ] = useState('');
+  const [ result, setResult ] = useState('');
 
 
   const handleChange = evt => {
@@ -18,14 +19,28 @@ export default function Converter() {
         let g = (bigint >> 8) & 255;
         let b = bigint & 255;
         setRgb('rgb('+r+','+g+ ','+b+')');
+        if(hex === '#000000') {
+          setResult('rgb(255,255,255)')
+        } else {
+          setResult('rgb('+(r - 80) +','+(g - 80) + ','+(b - 80)+')')
+        }
         setHex('')
       }
   	}
   	
   }
-
+  if(rgb === 'Ошибка!'){
+    return(<div className="result" style={{ backgroundColor: 'red' }}>
+      <input className="hex-input" type="text" value={hex} onChange={handleChange}/>
+      <div className="rgb-container" style={{ backgroundColor: result }}> 
+        <span className="">{rgb}</span>
+      </div>
+    </div> );
+  }
   return(<div className="result" style={{ backgroundColor: rgb }}>
   	  <input className="hex-input" type="text" value={hex} onChange={handleChange}/>
-      <span className="">{rgb}</span>
+      <div className="rgb-container" style={{ backgroundColor: result }}> 
+        <span className="">{rgb}</span>
+      </div>
   	</div> );
 }
